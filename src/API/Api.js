2dd41be49bb262181649fdf9d20cbc02563d4986
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-09-28 17:01:20 +0530

    Fetching the API cal using axios.

---
+++ src/API/Api.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+const API_KEY = process.env.REACT_APP_API_KEY;
+
+function Api() {
+    
+    const [apiData, getApiData] = useState({});
+
+    useEffect(() => {
+        const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${API_KEY}`;
+        axios.get(url)
+        .then(function(res) {
+            console.log(res.data);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+    }, []);
+    return (
+        <div>
+            {/* {apiData} */}
+        </div>
+    )
+}
+
+export default Api
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import ShowAPI from "./API/Api";
 
 function App() {
   return (
     <div className="App">
       Simulator
+      <ShowAPI/>
     </div>
   );
 }
