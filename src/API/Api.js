f78d4557a1aea823a69ceb048ed1775724aa5f03
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-09-29 17:39:27 +0530

    api: Use this for testing api.

--- src/API/Api.js
+++ src/API/Api.js
@@ -10,15 +10,17 @@ function Api() {
         const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${API_KEY}`;
         axios.get(url)
         .then(function(res) {
-            console.log(res.data);
+            console.log(res);
+            getApiData(res.data);
         })
         .catch(function(error) {
             console.log(error);
         });
     }, []);
+
     return (
         <div>
-            {/* {apiData} */}
+            
         </div>
     )
 }
