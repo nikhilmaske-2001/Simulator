168ac2fe525b87c6e3633921d520b1e4f6554ef8
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-09-29 18:38:03 +0530

    Added dummy data for reference.

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,17 +1,20 @@
+import { Card } from '@material-ui/core';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { CartesianGrid, Legend, Line, LineChart, XAxis, YAxis, Tooltip } from 'recharts';
+import { data } from './dummyData';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 function Api() {
     
-    const [apiData, getApiData] = useState({});
+    const [apiData, getApiData] = useState([{}]);
 
     useEffect(() => {
         const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${API_KEY}`;
         axios.get(url)
         .then(function(res) {
-            console.log(res);
-            getApiData(res.data);
+            getApiData(res);
+            console.log(apiData.data["Time Series (5min)"]);
         })
         .catch(function(error) {
             console.log(error);
@@ -19,9 +22,17 @@ function Api() {
     }, []);
 
     return (
-        <div>
-            
-        </div>
+        <Card >
+            <LineChart width={730} height={450} data={data}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis type="number" domain={[135, 139.27]}/>
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="invested" stroke="#8884d8" />
+            </LineChart>
+        </Card>
     )
 }
 
---
+++ src/API/dummyData.js
@@ -0,0 +1,106 @@
+export const data = [
+    {
+        "name" : "2001",
+        "invested": 139.17,
+        "level": 120,
+        "amt": 2001,
+    },
+    {
+        "name" : "2002",
+        "invested": 137.96,
+        "level": 125,
+        "amt": 2002,
+
+    },
+    {
+        "name" : "2003",
+        "invested": 137.03,
+        "level": 130,
+        "amt": 2003,
+
+    },
+    {
+        "name" : "2004",
+        "invested": 135.25,
+        "level": 135,
+        "amt": 2004,
+    },
+    {
+        "name" : "2005",
+        "invested": 139.17,
+        "level": 120,
+        "amt": 2001,
+    },
+    {
+        "name" : "2006",
+        "invested": 137.96,
+        "level": 125,
+        "amt": 2002,
+
+    },
+    {
+        "name" : "2007",
+        "invested": 137.03,
+        "level": 130,
+        "amt": 2003,
+
+    },
+    {
+        "name" : "2008",
+        "invested": 135.25,
+        "level": 135,
+        "amt": 2004,
+    },
+    {
+        "name" : "2009",
+        "invested": 139.17,
+        "level": 120,
+        "amt": 2001,
+    },
+    {
+        "name" : "2010",
+        "invested": 137.96,
+        "level": 125,
+        "amt": 2002,
+
+    },
+    {
+        "name" : "2011",
+        "invested": 137.03,
+        "level": 130,
+        "amt": 2003,
+
+    },
+    {
+        "name" : "2012",
+        "invested": 135.25,
+        "level": 135,
+        "amt": 2004,
+    },
+    {
+        "name" : "2013",
+        "invested": 139.17,
+        "level": 120,
+        "amt": 2001,
+    },
+    {
+        "name" : "2014",
+        "invested": 137.96,
+        "level": 125,
+        "amt": 2002,
+
+    },
+    {
+        "name" : "2015",
+        "invested": 137.03,
+        "level": 130,
+        "amt": 2003,
+
+    },
+    {
+        "name" : "2015",
+        "invested": 135.25,
+        "level": 135,
+        "amt": 2004,
+    },
+];
