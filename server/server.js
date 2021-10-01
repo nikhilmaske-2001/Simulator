184fed61ffb9a97a5de8b171f004ded4cc3fbcac
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-01 13:57:10 +0530

    connect: Connected db with mongoose.

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const password = process.env.MONGO_PASSWORD;
+
+const DB = `mongodb+srv://nikhil:<${password}>@sim.fnq0f.mongodb.net/sim?retryWrites=true&w=majority`
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+}).then(() => {
+    console.log('connection successfull');
+}).catch((error) => {
+    console.log(error);
+});
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
