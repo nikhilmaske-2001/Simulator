b1c43a823144804428e218677153a7e7ab59da36
Mike Pervolarakis <56447146+Pervolarakis@users.noreply.github.com>
2021-10-02 18:21:25 +0300

    login page: added js to trigger animation

--- client/src/pages/LoginPage/Login.js
+++ client/src/pages/LoginPage/Login.js
@@ -1,23 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 function Login() {
+
+  const [rightPanelActive, triggerRightPanelActive] = useState(false);
   return (
     <div>
       <h2>Sign in/up Form</h2>
-      <div class="container" id="container">
-        <div class="form-container sign-up-container">
+      <div className={`container ${(rightPanelActive?'right-panel-active':'')}`} id="container">
+        <div className="form-container sign-up-container">
           <form action="#">
             <h1>Create Account</h1>
-            <div class="social-container">
-              <a href="#" class="social">
-                <i class="fab fa-facebook-f"></i>
+            <div className="social-container">
+              <a href="#" className="social">
+                <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-google-plus-g"></i>
+              <a href="#" className="social">
+                <i className="fab fa-google-plus-g"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-linkedin-in"></i>
+              <a href="#" className="social">
+                <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
             <span>or use your email for registration</span>
@@ -27,42 +29,42 @@ function Login() {
             <button>Sign Up</button>
           </form>
         </div>
-        <div class="form-container sign-in-container">
+        <div className="form-container sign-in-container">
           <form action="#">
             <h1>Sign in</h1>
-            <div class="social-container">
-              <a href="#" class="social">
-                <i class="fab fa-facebook-f"></i>
+            <div className="social-container">
+              <a href="#" className="social">
+                <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-google-plus-g"></i>
+              <a href="#" className="social">
+                <i className="fab fa-google-plus-g"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-linkedin-in"></i>
+              <a href="#" className="social">
+                <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
             <span>or use your account</span>
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
             <a href="#">Forgot your password?</a>
-            <button>Sign In</button>
+            <button >Sign In</button>
           </form>
         </div>
-        <div class="overlay-container">
-          <div class="overlay">
-            <div class="overlay-panel overlay-left">
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
               <h1>Welcome Back!</h1>
               <p>
                 To keep connected with us please login with your personal info
               </p>
-              <button class="ghost" id="signIn">
+              <button className="ghost" id="signIn" onClick={()=>triggerRightPanelActive(false)}>
                 Sign In
               </button>
             </div>
-            <div class="overlay-panel overlay-right">
+            <div className="overlay-panel overlay-right">
               <h1>Hello, Friend!</h1>
               <p>Enter your personal details and start journey with us</p>
-              <button class="ghost" id="signUp">
+              <button className="ghost" id="signUp" onClick={()=>triggerRightPanelActive(true)}>
                 Sign Up
               </button>
             </div>
