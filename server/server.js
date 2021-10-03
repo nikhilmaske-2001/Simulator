2f52876b7959b4d2e31be91f21488cfb0621fd74
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-03 13:51:57 +0530

    server: Ran prettier.

--- server/server.js
+++ server/server.js
@@ -1,24 +1,27 @@
-const mongoose = require('mongoose');
-const express = require('express');
+const mongoose = require("mongoose");
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const password = process.env.REACT_APP_MONGO_PASSWORD;
 
 const DB = `mongodb+srv://nikhil:${password}@sim.fnq0f.mongodb.net/sim?retryWrites=true&w=majority`;
 
-mongoose.connect(DB, {
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
-}).then(() => {
-    console.log('connection successfull');
-}).catch((error) => {
+  })
+  .then(() => {
+    console.log("connection successfull");
+  })
+  .catch((error) => {
     console.log(error);
-});
+  });
 
 const connection = mongoose.connection;
-connection.once('open', () => {
+connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 });
