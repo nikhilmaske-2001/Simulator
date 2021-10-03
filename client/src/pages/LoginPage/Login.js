ece92ae73a6fabc367a6763f8454411afa8414f9
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-03 10:26:44 +0530

    login animation: Added the js code to support animation.

--- client/src/pages/LoginPage/Login.js
+++ client/src/pages/LoginPage/Login.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 
 function Login() {
+  const [rightPanelActive, setRightPanelActive] = useState(false);
+
   return (
     <div>
       <h2>Sign in/up Form</h2>
-      <div class="container" id="container">
-        <div class="form-container sign-up-container">
+      <div
+        className={`container ${rightPanelActive ? "right-panel-active" : ""}`}
+        id="container"
+      >
+        <div className="form-container sign-up-container">
           <form action="#">
             <h1>Create Account</h1>
-            <div class="social-container">
-              <a href="#" class="social">
+            <div className="social-container">
+              <a href="#" className="social">
                 <i class="fab fa-facebook-f"></i>
               </a>
-              <a href="#" class="social">
+              <a href="#" className="social">
                 <i class="fab fa-google-plus-g"></i>
               </a>
-              <a href="#" class="social">
+              <a href="#" className="social">
                 <i class="fab fa-linkedin-in"></i>
               </a>
             </div>
@@ -27,18 +32,18 @@ function Login() {
             <button>Sign Up</button>
           </form>
         </div>
-        <div class="form-container sign-in-container">
+        <div className="form-container sign-in-container">
           <form action="#">
             <h1>Sign in</h1>
-            <div class="social-container">
-              <a href="#" class="social">
-                <i class="fab fa-facebook-f"></i>
+            <div className="social-container">
+              <a href="#" className="social">
+                <i className="fab fa-facebook-f"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-google-plus-g"></i>
+              <a href="#" className="social">
+                <i className="fab fa-google-plus-g"></i>
               </a>
-              <a href="#" class="social">
-                <i class="fab fa-linkedin-in"></i>
+              <a href="#" className="social">
+                <i className="fab fa-linkedin-in"></i>
               </a>
             </div>
             <span>or use your account</span>
@@ -48,21 +53,29 @@ function Login() {
             <button>Sign In</button>
           </form>
         </div>
-        <div class="overlay-container">
-          <div class="overlay">
-            <div class="overlay-panel overlay-left">
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-left">
               <h1>Welcome Back!</h1>
               <p>
                 To keep connected with us please login with your personal info
               </p>
-              <button class="ghost" id="signIn">
+              <button
+                className="ghost"
+                id="signIn"
+                onClick={() => setRightPanelActive(false)}
+              >
                 Sign In
               </button>
             </div>
-            <div class="overlay-panel overlay-right">
+            <div className="overlay-panel overlay-right">
               <h1>Hello, Friend!</h1>
               <p>Enter your personal details and start journey with us</p>
-              <button class="ghost" id="signUp">
+              <button
+                className="ghost"
+                id="signUp"
+                onClick={() => setRightPanelActive(true)}
+              >
                 Sign Up
               </button>
             </div>
