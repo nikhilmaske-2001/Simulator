5577101eddc22465d0ae66de458dd4788d22838a
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-06 13:08:32 +0530

    server: Add middleware in server file.

--- server/server.js
+++ server/server.js
@@ -16,6 +16,19 @@ mongoose.connect(
   }
 );
 
-app.listen(process.env.PORT || 8800, () => {
-  console.log(`Server is running on port`);
+// Middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Home page");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Welcome to users page");
+});
+
+app.listen(8800, () => {
+  console.log(`Server is running on port: 8800`);
 });
