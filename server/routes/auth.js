d37b2ca40da4503e38c794c899141dff3b6b2e9b
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-06 15:02:53 +0530

    secure password: Using bcrypt to create hash password and hide in db.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,22 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 // Register
 router.post("/register", async (req, res) => {
-  const newUser = new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
-
   try {
+    // Generate new Hashed Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create new user
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+
+    // Save user in db
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
