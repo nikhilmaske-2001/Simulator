f4e9306c9ae3af96af59f1023ee2140e1ce5eea4
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-06 15:23:04 +0530

    server login: Add routes for login system.

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -20,7 +20,26 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (error) {
-    console.log(error);
+    res.status(500).json(error);
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    // Verifying email
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(404).json("user not found");
+
+    // Verifying password
+    const vaildPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    !vaildPassword && res.status(400).json("Wrong Password");
+    res.status(200).send(user);
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
