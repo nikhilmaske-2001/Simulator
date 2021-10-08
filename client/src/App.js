47c746c9420fe8010a827ad492071e6f162126c9
Nikhil Maske <nikhilmaske2001@gmail.com>
2021-10-08 11:14:17 +0530

    router: Add routes for login page.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import LoginPage from "./pages/LoginPage/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  return <LoginPage />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
